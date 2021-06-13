2d4c0a134960c9310b98c155043c225534674fae
Asad Baig <36822413+AsadBaig039@users.noreply.github.com>
2021-06-13 06:35:27 +0500

    CloudinaryImplemeted

---
+++ app/config/listingsData.js
@@ -0,0 +1,163 @@
+export const Listings = [
+  {
+    id: 201,
+    title: "Red jacket",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547388/jacket1_full_qgkgsc.jpg",
+        // url: "http://192.168.18.7:9000/assets/jacket1_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/jacket1_thumb.jpg",
+      },
+    ],
+    price: 100,
+    categoryId: 5,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 3,
+    title: "Gray couch in a great condition",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547390/couch3_full_vkqbo3.jpg",
+        // url: "http://192.168.18.7:9000/assets/couch2_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/couch2_thumb.jpg",
+      },
+    ],
+    categoryId: 1,
+    price: 1200,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 1,
+    title: "Room & Board couch (great condition) - delivery included",
+    description:
+      "I'm selling my furniture at a discount price. Pick up at Venice. DM me asap.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547392/couch1_full_bga9kh.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/couch1_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/couch1_thumb.jpg",
+      },
+      {
+        // url: "http://192.168.18.7:9000/assets/couch2_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/couch2_thumb.jpg",
+      },
+      {
+        // url: "http://192.168.18.7:9000/assets/couch3_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/couch3_thumb.jpg",
+      },
+    ],
+    price: 1000,
+    categoryId: 1,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 2,
+    title: "Designer wear shoes",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547387/shoes1_full_uzc2nu.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/shoes1_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/shoes1_thumb.jpg",
+      },
+    ],
+    categoryId: 5,
+    price: 100,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 102,
+    title: "Canon 400D (Great Condition)",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547386/camera1_full_e66cxg.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/camera1_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/camera1_thumb.jpg",
+      },
+    ],
+    price: 300,
+    categoryId: 3,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 101,
+    title: "Nikon D850 for sale",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547389/camera2_full_fkjmld.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/camera2_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/camera2_thumb.jpg",
+      },
+    ],
+    price: 350,
+    categoryId: 3,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 4,
+    title: "Sectional couch - Delivery available",
+    description: "No rips no stains no odors",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547387/couch2_full_pmt99x.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/couch3_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/couch3_thumb.jpg",
+      },
+    ],
+    categoryId: 1,
+    price: 950,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 6,
+    title: "Brown leather shoes",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dzcdsqxkb/image/upload/v1623547388/shoes2_full_gqjcbs.jpg",
+
+        // url: "http://192.168.18.7:9000/assets/shoes2_full.jpg",
+        thumbnailUrl: "http://192.168.18.7:9000/assets/shoes2_thumb.jpg",
+      },
+    ],
+    categoryId: 5,
+    price: 50,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+];
--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,34 +1,69 @@
-import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { Listings } from "../config/listingsData";
 
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 
+import listingsApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+
 function ListingScreen({ navigation }) {
-  const listings = [
-    {
-      id: 1,
-      title: "Red leather jacket",
-      price: "70$",
-      image: require("../assets/jacket.jpg"),
-    },
-    {
-      id: 2,
-      title: "Couch",
-      price: "300$",
-      image: require("../assets/couch.jpg"),
-    },
-    {
-      id: 3,
-      title: "Chair",
-      price: "120$",
-      image: require("../assets/chair.jpg"),
-    },
-  ];
+  const [listings, setListings] = useState(Listings);
+  // const [listings, setListings] = useState([]);
+  // const [error, setError] = useState(false);
+  // console.log(listings);
+
+  // const loadListings = async () => {
+  //   const response = await listingsApi.getListings();
+  //   if (!response.ok) return setError(true);
+
+  //   setError(false);
+  //   setListings(response.data);
+  // };
+
+  // useEffect(() => {
+  //   loadListings();
+  // }, []);
+  // const listings = [
+  //   {
+  //     id: 1,
+  //     title: "Red leather jacket",
+  //     price: "70$",
+  //     image: require("../assets/jacket.jpg"),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Couch",
+  //     price: "300$",
+  //     image: require("../assets/couch.jpg"),
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Chair",
+  //     price: "120$",
+  //     image: require("../assets/chair.jpg"),
+  //   },
+  // ];
   return (
     <Screen style={styles.screen}>
+      {/* {error && (
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <AppText>couldn't retrieve the listings</AppText>
+          <AppButton title="Retry" onPress={loadListings}></AppButton>
+        </View>
+      )} */}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -36,7 +71,7 @@ function ListingScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
